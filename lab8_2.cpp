81b2ab58ae9593731d9aff71e579bb9cabf4ac39
xchalarm <120165210+xchalarm@users.noreply.github.com>
2024-12-09 14:21:48 +0700

    lab8_2.cpp completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,6 +4,25 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double x){
+    
+    if(x>90){
+        return 'A';
+    }
+    else if(x>75){
+        return 'B';
+    }
+    else if(x>60){
+        return 'C';
+    }
+    else if(x>45){
+        return 'D';
+    }
+    else{
+        return 'F';
+    }
+    
+}
 
 int main(){
 	//Input the number of students
@@ -18,8 +37,10 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+        getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i] ;
 		i++;
 	}
 	
